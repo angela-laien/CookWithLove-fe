36d85dbbfc59e90b1b3c3a4aa89085d43df3772c
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-10-12 13:05:03 -0700

    added search bar

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 
 const AllRecipes = () => {
     const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState("")
+    const [filteredRecipes, setFilteredRecipes] = useState([]);
 
     useEffect(() => {
         axios 
@@ -15,10 +17,51 @@ const AllRecipes = () => {
             .catch((err) => console.log({err}));
     }, []);
 
+    useEffect(() => {
+        setFilteredRecipes(
+            recipes.filter((recipe) => 
+                recipe.recipeName.toLowerCase().includes(search.toLowerCase())
+            )
+        );
+    }, [search, recipes])
+
     return (
         <div className="all-container">
             <div data-aos="fade-in" data-aos-offset="200" data-aos-duration="1500">
                 <h1 className="all-title">👨‍🍳 Welcome to the Freestyle Kitchen 👩‍🍳 </h1>
+                <input className="search"
+                type="text"
+                placeholder="Search Recipes"
+                onChange={(e) => setSearch(e.target.value)}
+                />
+                {filteredRecipes.map((recipe) => (
+                    <div className='allRecipes' key={recipe.user_id}>
+                        <h2 className="recipeTitle">{recipe.recipeName}</h2>
+                        <Link to={`/recipes/${recipe.id}`}>
+                            {recipe.imageURL ? (
+                                <img 
+                                className='recipeImg'
+                                src={recipe.imageURL}
+                                alt='recipe'
+                                />
+                            ) : (
+                                <img 
+                                className='img'
+                                src='https://img.icons8.com/dusk/64/000000/no-camera.png'
+                                alt='not available'
+                                />
+                            )}
+                        </Link>
+                        
+                        <div className='combine'>
+                            <h3>Prep Time:</h3><p className="details">{recipe.prepTime}</p>
+                            <h3>Cook Time:</h3><p className="details">{recipe.cookTime}</p>
+                            </div>
+                        <h3>Serving:</h3><p className="details">{recipe.serving}</p>
+                        <h3>Ingredients: </h3><p className="details">{recipe.ingredients}</p>
+                        <h3>Instructions: </h3><p className="details">{recipe.instructions}</p>
+                    </div>
+                ))}
                 {recipes.map((recipe) => (
                     <div className='allRecipes' key={recipe.user_id}>
                         <h2 className="recipeTitle">{recipe.recipeName}</h2>
